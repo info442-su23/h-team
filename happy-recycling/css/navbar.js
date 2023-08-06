7b5ddecbb447d478c000f9a24b48e59389de6462
taylor-russon <twrusson@uw.edu>
2023-08-05 20:42:04 -0700

    fixed underline and button link

--- happy-recycling/css/navbar.js
+++ happy-recycling/css/navbar.js
@@ -10,3 +10,25 @@ document.querySelectorAll(".navbar-links a").forEach(n => n.addEventListener("cl
     hamburger.classList.remove("active");
     navbarLinks.classList.remove("active");
 }))
+
+
+
+//UNDERLINE
+
+const navbarUnderline = document.querySelector(".page-link");
+
+// Function to handle link click
+function handleLinkClick(event) {
+const clickedLink = event.target.closest("a");
+if (clickedLink) {
+    // Remove the "underlined" class from all links
+    navbarUnderline.querySelectorAll("a").forEach(link => link.classList.remove("underlined"));
+
+    // Add the "underlined" class to the clicked link
+    clickedLink.classList.add("underlined");
+}
+}
+
+// Add click event listener to the parent ul element
+navbarUnderline.addEventListener("click", handleLinkClick);
+
